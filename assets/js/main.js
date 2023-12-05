defd05ce2b95132cf4da940112de0c1c69191656
Laiz <laizsc.89@gmail.com>
2023-12-05 10:22:22 -0300

    Transformação dos dados da api em html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,20 @@
 
 function convertPokemonToLi(pokemon) {
-    return  `<li class="pokemon">
-                <span class="number">#001</span>
-                <span class="name">${pokemon.name}</span> 
-                <div class="detail">
-                    <ol class="types">
-                        <li class="type">grass</li>
-                        <li class="type">poison</li>
-                    </ol>
+    return `
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
 
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
-                </div>
-            </li>`
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+
+                <img src="${pokemon.photo}"
+                     alt="${pokemon.name}">
+            </div>
+        </li>
+    `
 }
 
 const pokemonList = document.getElementById('pokemonList');
