1246db87ead86def81acd940285d9eb54a6cc47a
Laiz <laizsc.89@gmail.com>
2023-12-05 10:52:09 -0300

    Implementação do button loadPokemon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
+const pokemonList = document.getElementById('pokemonList');
+const loadMoreButton = document.getElementById('loadMoreButton');
+const maxRecords = 151
+const limit = 5;
+let offset = 0;
+
+
 
 function convertPokemonToLi(pokemon) {
     return `
@@ -16,12 +23,27 @@ function convertPokemonToLi(pokemon) {
         </li>
     `
 }
-
-const pokemonList = document.getElementById('pokemonList');
-
-pokeApi.getPokemons().then((pokemons = []) => {
+function loadPokemonItens(offset,limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map(convertPokemonToLi).join('')
+        pokemonList.innerHTML += newHtml
     
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+        })
+}
 
-    })
+loadPokemonItens(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    const qtdRecordsWithNexPage = offset + limit
+
+    if (qtdRecordsWithNexPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItens(offset, limit)
+    }
+})
 
